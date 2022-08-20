27909c2c57ed86ba016c1575d0e6e7ac1e38346a
Achyuth Varma <achyuth.varma1@gmail.com>
2022-08-20 19:00:48 -0400

    Added all POJO classes

---
+++ src/main/java/com/todoapp/userauth/repository/UserRepo.java
@@ -0,0 +1,10 @@
+package com.todoapp.userauth.repository;
+
+import com.todoapp.userauth.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
