bbdf09c78c82e175ad49c34f300c2336cb2929f9
Achyuth Varma <achyuth.varma1@gmail.com>
2022-08-22 16:40:47 -0400

    Added user signin method

---
+++ src/main/java/com/todoapp/userauth/dto/UserSignIn.java
@@ -0,0 +1,12 @@
+package com.todoapp.userauth.dto;
+
+import lombok.Data;
+
+@Data
+public class UserSignIn {
+
+    private String username;
+
+    private String password;
+
+}
--- src/main/java/com/todoapp/userauth/service/UserService.java
+++ src/main/java/com/todoapp/userauth/service/UserService.java
@@ -1,9 +1,13 @@
 package com.todoapp.userauth.service;
 
 
+import com.todoapp.userauth.Exceptions.InvalidUsernameOrPasswordException;
+import com.todoapp.userauth.dto.UserSignIn;
+import com.todoapp.userauth.dto.UserSignInOutput;
 import com.todoapp.userauth.dto.UserSignUp;
 import com.todoapp.userauth.models.User;
 import com.todoapp.userauth.repository.UserRepo;
+import com.todoapp.userauth.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,9 @@ public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
@@ -26,6 +33,26 @@ public class UserService {
         user.setPassword(password);
         return userRepo.save(user);
 
+    }
+
+
+    public UserSignInOutput userSignIn(UserSignIn userSignIn) throws  RuntimeException{
+
+        User user = userRepo.findByUsername(userSignIn.getUsername());
+
+        UserSignInOutput userSignInOutput = new UserSignInOutput();
+
+        if(bCryptPasswordEncoder.matches(userSignIn.getPassword(), user.getPassword())){
+            userSignInOutput.setUserId(user.getUserId());
+            userSignInOutput.setUsername(user.getUsername());
+            String jwtToken = jwtUtil.generateToken(String.valueOf(user.getUserId()));
+            userSignInOutput.setGeneratedToken(jwtToken);
+        }
+        else{
+            throw new InvalidUsernameOrPasswordException("You've given invalid username and password");
+        }
+
+        return userSignInOutput;
     }
 
 
